91337ab670639292ddafbf4a3a9906fefe8f9040
Fernando del Pino <fherdelpino@hotmail.com>
2018-08-26 20:35:58 -0500

    Alimazon C3 v3

---
+++ alimazonC3/src/main/scala/AlimazonC3v3.scala
@@ -0,0 +1,55 @@
+/* AlimazonC3v3.scala */
+import org.apache.spark.sql.{SparkSession,Dataset,Row,SaveMode}
+import org.apache.spark.sql.types.{StructType, StringType, TimestampType, IntegerType, DoubleType}
+import java.util.Calendar
+import java.sql.Timestamp
+
+object AlimazonC3v3 {
+    
+    def main(args: Array[String]) {
+        val spark = SparkSession.builder().appName("alimazonC3").getOrCreate()
+        import spark.implicits._
+        
+        val schema = (new StructType).add("id", StringType).add("client_id",StringType).add("timestamp", TimestampType).add("product_id",StringType).add("quantity",IntegerType).add("total",DoubleType)
+
+        val orders = spark.read.schema(schema).json("gs://de-training-input/alimazon/200000/client-orders/*.jsonl.gz")
+
+        orders.createOrReplaceTempView("orders")
+
+        spark.udf.register("dayofweek", (input: Timestamp) => {
+            val cal = Calendar.getInstance()
+            cal.setTime(input)
+            cal.get(Calendar.DAY_OF_WEEK)
+        })
+
+        //Function to save to a file in CVS format
+        def saveToFile(ds:Dataset[Row],filename:String) = {
+            ds.write.mode(SaveMode.Overwrite)
+               .format("com.databricks.spark.csv")
+               .option("header", true)
+               .option("delimiter", ",")
+               .save(s"gs://de-training-output-fherdelpino/assignment-3/$filename" + "2")
+        }
+
+        def runProductReports(i:Int) : Unit = {
+            val productWeek = spark.sql(s"select product_id, sum(total) gross_sales, count(id) orders_count from orders where dayofweek(timestamp) = $i group by product_id")
+            productWeek.cache()
+            saveToFile(productWeek.orderBy($"gross_sales".desc).limit(10),s"top10_products_by_gross_sales_$i")
+            saveToFile(productWeek.orderBy($"orders_count".desc).limit(10),s"top10_products_by_orders_$i")
+            println(s"Product report $i done")
+        }
+
+        def runCustomerReports(i:Int) : Unit = {
+            val customerMonth = spark.sql(s"select client_id, sum(total) gross_sales, count(id) orders_count from orders where month(timestamp) = $i group by client_id")
+            customerMonth.cache()
+            saveToFile(customerMonth.orderBy($"gross_sales".desc).limit(10),s"top10_customers_by_gross_spending_$i")
+            saveToFile(customerMonth.orderBy($"orders_count".desc).limit(10),s"top10_customers_by_orders_$i")
+            println(s"Customers report $i done")
+        }
+
+        for (i <- 1 to 7) runProductReports(i)
+
+        for (i <- 1 to 12) runCustomerReports(i)
+
+    }
+}
