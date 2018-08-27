bc134af076f56f6b5d3b298afbabebe347589342
Fernando del Pino <fherdelpino@hotmail.com>
2018-08-26 23:41:33 -0500

    alimazon c3 select correct fields to show

--- alimazonC3/src/main/scala/AlimazonC3v3.scala
+++ alimazonC3/src/main/scala/AlimazonC3v3.scala
@@ -7,6 +7,7 @@ import java.sql.Timestamp
 object AlimazonC3v3 {
     
     def main(args: Array[String]) {
+        println("Starting program...")
         val spark = SparkSession.builder().appName("alimazonC3").getOrCreate()
         import spark.implicits._
         
@@ -16,6 +17,7 @@ object AlimazonC3v3 {
 
         orders.createOrReplaceTempView("orders")
 
+        println("Defining functions")
         spark.udf.register("dayofweek", (input: Timestamp) => {
             val cal = Calendar.getInstance()
             cal.setTime(input)
@@ -28,28 +30,31 @@ object AlimazonC3v3 {
                .format("com.databricks.spark.csv")
                .option("header", true)
                .option("delimiter", ",")
-               .save(s"gs://de-training-output-fherdelpino/assignment-3/$filename" + "2")
+               .save(s"gs://de-training-output-fherdelpino/assignment-3/$filename" + "-2")
         }
 
         def runProductReports(i:Int) : Unit = {
             val productWeek = spark.sql(s"select product_id, sum(total) gross_sales, count(id) orders_count from orders where dayofweek(timestamp) = $i group by product_id")
             productWeek.cache()
-            saveToFile(productWeek.orderBy($"gross_sales".desc).limit(10),s"top10_products_by_gross_sales_$i")
-            saveToFile(productWeek.orderBy($"orders_count".desc).limit(10),s"top10_products_by_orders_$i")
+            saveToFile(productWeek.select("product_id","gross_sales").orderBy($"gross_sales".desc).limit(10),s"top10_products_by_gross_sales_$i")
+            saveToFile(productWeek.select("product_id","orders_count").orderBy($"orders_count".desc).limit(10),s"top10_products_by_orders_$i")
             println(s"Product report $i done")
         }
 
         def runCustomerReports(i:Int) : Unit = {
             val customerMonth = spark.sql(s"select client_id, sum(total) gross_sales, count(id) orders_count from orders where month(timestamp) = $i group by client_id")
             customerMonth.cache()
-            saveToFile(customerMonth.orderBy($"gross_sales".desc).limit(10),s"top10_customers_by_gross_spending_$i")
-            saveToFile(customerMonth.orderBy($"orders_count".desc).limit(10),s"top10_customers_by_orders_$i")
+            saveToFile(customerMonth.select("client_id","gross_sales").orderBy($"gross_sales".desc).limit(10),s"top10_customers_by_gross_spending_$i")
+            saveToFile(customerMonth.select("client_id","orders_count").orderBy($"orders_count".desc).limit(10),s"top10_customers_by_orders_$i")
             println(s"Customers report $i done")
         }
 
+        println("Starting reports")
         for (i <- 1 to 7) runProductReports(i)
 
         for (i <- 1 to 12) runCustomerReports(i)
+
+        println("Program terminated")
 
     }
 }
